f46cd4dc83b6144d68443b6f8a6aed27981189fa
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2019-04-12 09:31:20 -0700

    code shuffle to put stuff in better places

--- vonigo.go
+++ vonigo.go
@@ -3,11 +3,6 @@ package vonigo
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strconv"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -48,85 +43,6 @@ func Init(c Config) error {
 
 	isInitialized = true
 	return nil
-}
-
-// ***** Clients ***** //
-
-// GetClients - Get all clients
-func GetClients(params map[string]string) ([]Client, error) {
-	clients := []Client{}
-	clientResponse := ClientsResponse{}
-
-	if !hasSecurityToken() {
-		err := getSecurityToken()
-		if err != nil {
-			return clients, err
-		}
-	}
-
-	mergedParams, _ := getBaseParams("retrieve")
-
-	for i, item := range params {
-
-		if i == "minutes" {
-			start, end := getMinutesAgo(item)
-			mergedParams["dateStart"] = start
-			mergedParams["dateEnd"] = end
-		}
-		mergedParams[i] = item
-	}
-
-	reqURL, reqParams, err := buildURL(baseURL, "api/v1/data/Clients", mergedParams)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := http.PostForm(reqURL, reqParams)
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	err = json.Unmarshal(body, &clientResponse)
-	if err != nil {
-		return nil, err
-	}
-	return clientResponse.Clients, nil
-}
-
-// GetClient - Get a single client
-func GetClient(id int) (Client, error) {
-	stringID := strconv.Itoa(id)
-	client := Client{}
-	clientResponse := ClientResponse{}
-
-	if !hasSecurityToken() {
-		err := getSecurityToken()
-		if err != nil {
-			return client, err
-		}
-	}
-	params, _ := getBaseParams("retrieve")
-	params["objectID"] = stringID
-
-	reqURL, urlValues, err := buildURL(baseURL, "api/v1/data/Clients", params)
-	if err != nil {
-		return client, err
-	}
-	fmt.Println(reqURL)
-	resp, err := http.PostForm(reqURL, urlValues)
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	err = checkVonigoError(body)
-
-	if err != nil {
-		return client, err
-	}
-
-	err = json.Unmarshal(body, &clientResponse)
-	if err != nil {
-		return client, err
-	}
-	return clientResponse.Client, nil
 }
 
 // ***** Leads ***** //
